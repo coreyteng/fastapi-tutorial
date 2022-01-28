fb26fee4b2e4eb77803619da704b427ce733d353
coreyteng <oh8048@gmail.com>
2022-01-28 18:01:39 +0800

    C3

--- run.py
+++ run.py
@@ -8,8 +8,9 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
-from coronavirus import application
-from tutorial import app03, app04, app05, app06, app07, app08
+from tutorial import app03
+# from coronavirus import application
+# from tutorial import app04, app05, app06, app07, app08
 
 # from fastapi.exceptions import RequestValidationError
 # from fastapi.responses import PlainTextResponse
@@ -68,12 +69,12 @@ app.add_middleware(
 )
 
 app.include_router(app03, prefix='/chapter03', tags=['第三章 请求参数和验证'])
-app.include_router(app04, prefix='/chapter04', tags=['第四章 响应处理和FastAPI配置'])
-app.include_router(app05, prefix='/chapter05', tags=['第五章 FastAPI的依赖注入系统'])
-app.include_router(app06, prefix='/chapter06', tags=['第六章 安全、认证和授权'])
-app.include_router(app07, prefix='/chapter07', tags=['第七章 FastAPI的数据库操作和多应用的目录结构设计'])
-app.include_router(app08, prefix='/chapter08', tags=['第八章 中间件、CORS、后台任务、测试用例'])
-app.include_router(application, prefix='/coronavirus', tags=['新冠病毒疫情跟踪器API'])
+# app.include_router(app04, prefix='/chapter04', tags=['第四章 响应处理和FastAPI配置'])
+# app.include_router(app05, prefix='/chapter05', tags=['第五章 FastAPI的依赖注入系统'])
+# app.include_router(app06, prefix='/chapter06', tags=['第六章 安全、认证和授权'])
+# app.include_router(app07, prefix='/chapter07', tags=['第七章 FastAPI的数据库操作和多应用的目录结构设计'])
+# app.include_router(app08, prefix='/chapter08', tags=['第八章 中间件、CORS、后台任务、测试用例'])
+# app.include_router(application, prefix='/coronavirus', tags=['新冠病毒疫情跟踪器API'])
 
 if __name__ == '__main__':
-    uvicorn.run('run:app', host='0.0.0.0', port=8000, reload=True, debug=True, workers=1)
+    uvicorn.run('run:app', host='0.0.0.0', port=8000, reload=True, debug=True, workers=8)
